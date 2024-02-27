50564bdbefd40dea8d88e74016d2efa6b73ed52d
JoxonXaker <joxonxaker@gmail.com>
2024-02-27 23:04:33 +0500

    delete index file

--- project/settings.py
+++ project/settings.py
@@ -18,12 +18,13 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 CSRF_TRUSTED_ORIGINS = [
     # '*',
+    '.vercel.app'
 ]
 
 # Application definition
