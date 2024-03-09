dac9e77cf17e3599bd989d152fbcc2a5433feb0e
JoxonXaker <joxonxaker@gmail.com>
2024-03-10 00:06:25 +0500

    some changes

--- project/settings.py
+++ project/settings.py
@@ -1,24 +1,22 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
-import environ
 
-env = environ.Env(
-    DEBUG=(bool, True)
-)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+load_dotenv(os.path.join(BASE_DIR/".eVar", ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS").split()
 
 CORS_ORIGIN_ALLOW_ALL = True
 
